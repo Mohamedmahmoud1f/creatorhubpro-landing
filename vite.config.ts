a7ffaf219762abdb33255ffbacd43fc28c00b35c
Amir Alsayed <amyralsyd367@gmail.com>
2026-03-14 00:44:26 +0200

    configuration for vercel and vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import devServer from '@hono/vite-dev-server'
 import { defineConfig } from 'vite'
+import build from '@hono/vite-build/vercel'
 
 export default defineConfig({
   plugins: [
     devServer({
       entry: 'src/index.tsx'
-    })
+    }),
+    build()
   ]
 })
